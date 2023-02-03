a8ac28dcbf7a95ae01c3e1742ee89ed01c232550
core <107654857+corelorax@users.noreply.github.com>
2023-02-03 11:55:35 -0500

    Update Splix.lua

--- Splix.lua
+++ Splix.lua
@@ -1,4 +1,4 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/Splix"))()
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/corelorax/Ui-Libs-Backups/main/SplixSource.lua"))()
 
 local window = library:new({textsize = 13.5,font = Enum.Font.RobotoMono,name = "YES",color = Color3.fromRGB(225,58,81)})
 
